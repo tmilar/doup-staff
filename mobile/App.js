635af3d480df31a1050d103a1e71b52ffe05a401
Tomas Milar <milar@strands.com>
2019-03-09 02:26:19 -0300

    mobile: extract Uploader component and simplify main App component.

--- mobile/App.js
+++ mobile/App.js
@@ -1,208 +1,22 @@
-import React, { Component } from 'react';
-import {
-  ActivityIndicator,
-  Button,
-  Clipboard,
-  Image,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import { Constants, ImagePicker, Permissions } from 'expo';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import Uploader from './components/Uploader'
 
 export default class App extends Component {
-  state = {
-    image: null,
-    uploading: false,
-  };
-
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="default" />
-
-        <Text
-          style={styles.exampleText}>
-          Example: Upload ImagePicker result
-        </Text>
-
-        <Button onPress={this._takePhoto} title="Tomar foto" />
-
-        {this._maybeRenderImage()}
-        {this._maybeRenderUploadingOverlay()}
+        <Uploader/>
       </View>
     );
   }
-
-  _maybeRenderUploadingOverlay = () => {
-    if (this.state.uploading) {
-      return (
-        <View
-          style={[StyleSheet.absoluteFill, styles.maybeRenderUploading]}>
-          <ActivityIndicator color="#fff" size="large" />
-        </View>
-      );
-    }
-  };
-
-  _maybeRenderImage = () => {
-    let {
-      image
-    } = this.state;
-
-    if (!image) {
-      return;
-    }
-
-    return (
-      <View
-        style={styles.maybeRenderContainer}>
-        <View
-          style={styles.maybeRenderImageContainer}>
-          <Image source={{ uri: image }} style={styles.maybeRenderImage} />
-        </View>
-
-        <Text
-          onPress={this._copyToClipboard}
-          style={styles.maybeRenderImageText}>
-          {image}
-        </Text>
-      </View>
-    );
-  };
-
-  _copyToClipboard = () => {
-    Clipboard.setString(this.state.image);
-    alert('Copied image URL to clipboard');
-  };
-
-  _takePhoto = async () => {
-    const {
-      status: cameraPerm
-    } = await Permissions.askAsync(Permissions.CAMERA);
-
-    const {
-      status: cameraRollPerm
-    } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-    // only if user allows permission to camera AND camera roll
-    if (cameraPerm === 'granted' && cameraRollPerm === 'granted') {
-      let pickerResult = await ImagePicker.launchCameraAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
-      });
-
-      return this._handleImagePicked(pickerResult);
-    }
-  };
-
-  _handleImagePicked = async pickerResult => {
-    let uploadResponse, uploadResult;
-
-    try {
-      this.setState({
-        uploading: true
-      });
-
-      if (!pickerResult.cancelled) {
-        uploadResponse = await uploadImageAsync(pickerResult.uri);
-        uploadResult = await uploadResponse.json();
-
-        this.setState({
-          image: uploadResult.uploadFilename
-        });
-      }
-    } catch (e) {
-      console.log({ uploadResponse });
-      console.log({ uploadResult });
-      console.log({ e });
-      alert(`Error en la subida, por favor intente de nuevo. \n${JSON.stringify(e)}`);
-    } finally {
-      this.setState({
-        uploading: false
-      });
-    }
-  };
-}
-
-async function uploadImageAsync(uri) {
-  const resource = '/upload'
-  let apiUrl = 'https://file-upload-example-backend-dkhqoilqqn.now.sh'
-
-  // Note:
-  // Uncomment this if you want to experiment with local server
-  // if (Constants.isDevice) {
-  //   const ngrok = 'http://93cedb6c.ngrok.io'
-  //   apiUrl = ngrok
-  // } else {
-  //   apiUrl = 'http://localhost:3000'
-  // }
-  const pathUrl = `${apiUrl}${resource}`;
-
-  let uriParts = uri.split('.');
-  let fileType = uriParts[uriParts.length - 1];
-
-  let formData = new FormData();
-  formData.append('photo', {
-    uri,
-    name: `photo.${fileType}`,
-    type: `image/${fileType}`,
-  });
-
-  let options = {
-    method: 'POST',
-    body: formData,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'multipart/form-data',
-    },
-  };
-
-  return fetch(pathUrl, options);
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
-  },
-  exampleText: {
-    fontSize: 20,
-    marginBottom: 20,
-    marginHorizontal: 15,
-    textAlign: 'center',
-  },
-  maybeRenderUploading: {
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.4)',
-    justifyContent: 'center',
-  },
-  maybeRenderContainer: {
-    borderRadius: 3,
-    elevation: 2,
-    marginTop: 30,
-    shadowColor: 'rgba(0,0,0,1)',
-    shadowOpacity: 0.2,
-    shadowOffset: {
-      height: 4,
-      width: 4,
-    },
-    shadowRadius: 5,
-    width: 250,
-  },
-  maybeRenderImageContainer: {
-    borderTopLeftRadius: 3,
-    borderTopRightRadius: 3,
-    overflow: 'hidden',
-  },
-  maybeRenderImage: {
-    height: 250,
-    width: 250,
-  },
-  maybeRenderImageText: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
+    justifyContent: 'center'
   }
 });
