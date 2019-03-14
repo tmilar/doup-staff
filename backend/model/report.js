dcd8bacf398af0d9f6725b0bdf063af122f1e244
Tomas Milar <milar@strands.com>
2019-03-14 01:50:42 -0300

    backend: stop saving Report 'updatedAt' field, since it's unneeded for now.

--- backend/model/report.js
+++ backend/model/report.js
@@ -5,7 +5,7 @@ const ReportSchema = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   comment: {type: String, trim: true}
 }, {
-  timestamps: true
+  timestamps: {createdAt: true, updatedAt: false}
 })
 
 module.exports = mongoose.model('Report', ReportSchema)
