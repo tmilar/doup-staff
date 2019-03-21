8bd2f18938c865206378ce11d09a845a5040feed
tmilar <tomasmilar4@gmail.com>
2019-03-21 15:59:50 +0100

    backend: properly check valid user when sending a report.

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -9,20 +9,26 @@ function isValidDateParam(dateStr) {
   if(!dateParam instanceof Date) {
     return false
   }
-  const time3MinutesMillis = 1000*60*3
+  const time5MinutesMillis = 1000*60*5
   const dateDiffMillis = new Date() - dateParam
-  return dateDiffMillis > time3MinutesMillis
+  return dateDiffMillis < time5MinutesMillis
 }
 
 router.post('/', isAuthorized, async (req, res) => {
   const {username} = req
   const {date} = req.body
   if(!isValidDateParam(date)) {
-    return res.status(401)
-      .json({status: 401, error: `param 'date' is invalid: '${date}'`})
+    return res.status(400)
+      .json({status: 400, message: `El cliente ingreso una fecha invalida: '${date}'`})
   }
 
   const reportingUser = await User.findOne({username})
+  if(!reportingUser) {
+    return res.status(400)
+      .json({status: 400, message: `Usuario invalido: ${username}, por favor inicie sesion nuevamente.`})
+  }
+
+  // create a new report, and send it via mail.
   const {user, date: reportDate} = await Report.create({user: reportingUser, date})
 
   await sendReport({user, date: reportDate})
