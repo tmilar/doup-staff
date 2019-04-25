20c9d8ee4d53078b2cd34c24677f003aa0d9c472
tmilar <tomasmilar4@gmail.com>
2019-04-25 17:35:41 -0300

    backend: is-authorized middleware throw error instead of returning response.

--- backend/lib/is-authorized.js
+++ backend/lib/is-authorized.js
@@ -11,41 +11,43 @@ async function isAuthorized(req, res, next) {
     req.cookies.token
 
   if (!token || token === 'null') {
-    logger.info('Unauthorized: No token provided.')
-    return res
-      .status(401)
-      .json({status: 401, message: 'El usuario no esta autorizado'})
+    logger.error('Unauthorized: No token provided.')
+    const error = new Error('El usuario no esta autorizado')
+    error.status = 401
+    return next(error)
   }
 
   let decoded
   try {
     decoded = await jwt.verify(token, authSecret)
   } catch (error) {
-    logger.error('Unauthorized: Invalid token.')
-    return res
-      .status(401)
-      .json({status: 401, message: 'La sesion expiro, por favor vuelva a ingresar'})
+    logger.error(`Unauthorized: Invalid token. Msg: ${error.message}`)
+    error.message = 'La sesion expiro, por favor vuelva a ingresar'
+    error.status = 401
+    return next(error)
   }
 
   // User is logged in
   const {username} = decoded
   req.username = username
 
+  logger.info(`Request from User: '${username}'`)
+
   let user
   try {
     user = await User.findOne({username})
   } catch (error) {
     logger.error(`Authorization error: problem retrieving user ${username} from DB. `, error)
-    return res
-      .status(401)
-      .json({status: 401, message: 'La sesion expiro, por favor vuelva a ingresar'})
+    error.message = 'La sesion expiro, por favor vuelva a ingresar'
+    error.status = 401
+    return next(error)
   }
 
   if (!user) {
     logger.error(`Authorization error: could not find user ${username} in DB.`)
-    return res
-      .status(401)
-      .json({status: 401, message: 'El nombre de usuario es inválido, por favor vuelva a ingresar.'})
+    error.message = 'El nombre de usuario es inválido, por favor vuelva a intentarlo. '
+    error.status = 401
+    return next(error)
   }
 
   req.user = user
