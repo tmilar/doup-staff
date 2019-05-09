db9209ca3ad6a9f1a42e66e4cae17ebb4713cc4b
tmilar <tomasmilar4@gmail.com>
2019-05-09 17:26:57 -0300

    mobile: small refactor in LoadingScreen, for a more clean flow.

--- mobile/screens/LoadingScreen.js
+++ mobile/screens/LoadingScreen.js
@@ -8,26 +8,35 @@ export default class LoadingScreen extends React.Component {
   async componentDidMount() {
     const {navigation} = this.props
 
+    const nextScreen = await this._evaluateNextScreen()
+    console.log(`[LoadingScreen] Navigating to ${nextScreen}.`)
+    navigation.navigate(nextScreen)
+  };
+
+  /**
+   * Evaluate next screen to navigate based on current data status evaluation.
+   *
+   * @returns {Promise<string>} - next screen name
+   * @private
+   */
+  _evaluateNextScreen = async () => {
     if (!await this._checkLogin()) {
       // switch to the Auth screen and unmount this loading screen away.
-      console.log("[LoadingScreen] No login, navigating to 'Auth'.")
-      navigation.navigate('Auth')
-      return
+      console.log("[LoadingScreen] No login.")
+      return 'Auth'
     }
     console.log("[LoadingScreen] User is logged in.")
 
-    if (!await this._checkRegisterForPushNotifications()) {
-      // switch to the Auth screen and unmount this loading screen away.
+    if (!await this._checkOrRegisterForPushNotifications()) {
+      // switch to the Auth screen, clearing storage as well (simulating a log-out).
       console.log("[LoadingScreen] Register for push notifications failed, navigating to 'Auth'.")
       await AsyncStorage.clear()
-      navigation.navigate('Auth')
-      return
+      return 'Auth'
     }
     console.log("[LoadingScreen] Device is registered for push notifications.")
 
-    const lessonScreen = await this._checkCurrentLesson()
-    navigation.navigate(lessonScreen)
-  };
+    return await this._checkCurrentLesson()
+  }
 
   /**
    * Check login.
@@ -43,11 +52,11 @@ export default class LoadingScreen extends React.Component {
    * Check if device/installation is registered for push notifications for the user.
    * If not, try to register.
    *
-   * @returns {Promise<boolean>}
+   * @returns {Promise<boolean>} - true if registered
    * @private
    */
-  _checkRegisterForPushNotifications = async () => {
-    if(await PushNotificationsService.isDeviceRegistered()) {
+  _checkOrRegisterForPushNotifications = async () => {
+    if (await PushNotificationsService.isDeviceRegistered()) {
       return true
     }
 
@@ -73,23 +82,23 @@ export default class LoadingScreen extends React.Component {
     const lesson = await LessonService.retrieveNextLesson()
 
     if (!lesson) {
-      console.log("[LoadingScreen] No next lesson, navigating to 'App'")
+      console.log("[LoadingScreen] No next lesson available.")
       return 'App'
     }
 
     const isUpcoming = LessonService.isUpcoming(lesson) // still not started, and is startable
-    if(isUpcoming) {
-      console.log("[LoadingScreen] Next lesson has not been started yet, navigating to 'UpcomingLesson'")
+    if (isUpcoming) {
+      console.log("[LoadingScreen] Next lesson has not been started yet.")
       return 'UpcomingLesson'
     }
 
     const isCurrent = LessonService.isCurrent(lesson) // started, but still not finished.
-    if(isCurrent) {
-      console.log("[LoadingScreen] Lesson has already been started, and not yet finished, navigating to 'CurrentLesson'")
+    if (isCurrent) {
+      console.log("[LoadingScreen] Lesson has already been started, and not yet finished. ")
       return 'CurrentLesson'
     }
 
-    console.error("[LoadingScreen] lesson is not upcoming and not current... navigating to 'App'")
+    console.error("[LoadingScreen] lesson is NOT upcoming and NOT current...")
     return 'App'
   }
 
